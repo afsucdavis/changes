5258745b78891eec1846003133976447d9b13069
HK <afsucdavis@gmail.com>
2019-08-21 15:37:11 -0700

    Initial commit

---
+++ story_content/5d51EvzBhb1_22050_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.27.18822.0%0D%0A%0D%0A00:00:00.064%20--%3E%2000:00:00.928%0D%0AAnd%20more%20changes...%0D%0A%0D%0A"}')
